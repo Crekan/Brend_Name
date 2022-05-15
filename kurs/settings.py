ea214b314de07d9485416c5c1b300899e96726cd
vaniakazeko <vania.kazeko.00@bk.ru>
2022-05-15 18:50:32 +0300

    2

--- kurs/settings.py
+++ kurs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hv^7m^$0t(3(&0!jwki+e*)j=r9zw*xhi1($(i=!=v#+o*#36)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
